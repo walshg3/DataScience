4a059cc69de4bdfa65fcdb0a648a817e51fd268e
Greg Walsh <walshg3@go.stockton.edu>
2019-09-23 20:15:16 -0400

    changes to mini 1

--- Mini 1 /compute_total_cost_greg_walsh.py
+++ Mini 1 /compute_total_cost_greg_walsh.py
@@ -6,19 +6,15 @@ Applies an age filter to the dataset in the form of a minimum and maximum inclus
 Applies a region filter to the dataset in the form of a comma separated string
 Returns two values: the number of entries in the filtered subset and the average charge from the subset
 """
-
 def compute_avg_charges(filename, min_age, max_age, regions):
   import pandas as pd
+  #create new DataFrame
   df = pd.read_csv(filename)
-  sf = df[
-    (df.age >= min_age)
-    & 
-    (df.age <= max_age)
-    &
-    (df.region.isin(regions.split(",")))
-  ]
-  return ( len(sf), sf.charges.mean() )
-
-
-def compute_avg_charges_test(filename,min_age, max_age, regions):
-  
+  #Applies an age filter to the dataset in the form of a minimum and maximum inclusive age
+  minFilter = df.age >= min_age
+  maxFilter = df.age <= max_age
+  #Applies a region filter to the dataset in the form of a comma separated string
+  regionFilter = df.region.isin(regions.split(","))
+  #Combine all Filters into one final filter to return 
+  finalFilter = df[minFilter & maxFilter & regionFilter]
+  return (len(finalFilter), finalFilter.charges.mean())
