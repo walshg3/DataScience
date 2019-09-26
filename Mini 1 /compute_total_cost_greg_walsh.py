393a0952f28e9f4922ea2dada7432590fc3652f9
Greg Walsh <walshg3@go.stockton.edu>
2019-09-26 02:36:23 -0400

    before comments

--- Mini 1 /compute_total_cost_greg_walsh.py
+++ Mini 1 /compute_total_cost_greg_walsh.py
@@ -1,20 +1,10 @@
-""" 
-Submit a Python code which supports a function that does the following:
-
-Accepts a filename as a variable input
-Applies an age filter to the dataset in the form of a minimum and maximum inclusive age
-Applies a region filter to the dataset in the form of a comma separated string
-Returns two values: the number of entries in the filtered subset and the average charge from the subset
-"""
 def compute_avg_charges(filename, min_age, max_age, regions):
-  import pandas as pd
-  #create new DataFrame
-  df = pd.read_csv(filename)
-  #Applies an age filter to the dataset in the form of a minimum and maximum inclusive age
-  minFilter = df.age >= min_age
-  maxFilter = df.age <= max_age
-  #Applies a region filter to the dataset in the form of a comma separated string
-  regionFilter = df.region.isin(regions.split(","))
-  #Combine all Filters into one final filter to return 
-  finalFilter = df[minFilter & maxFilter & regionFilter]
-  return (len(finalFilter), finalFilter.charges.mean())
+  with open(filename) as csv_file:
+    charges = []
+    next(csv_file)
+    for row in csv_file:
+      line = row.split(',')
+      if int(line[0]) >= min_age and int(line[0]) <= max_age and line[5] in regions.split(","):
+        charges.append(line[6].rstrip())
+    charges = [float(i) for i in charges]
+  return(len(charges), sum(charges)/len(charges))
