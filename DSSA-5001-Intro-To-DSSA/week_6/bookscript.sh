ffeb3e6ad52263cbec01183041120ee095e67b15
Greg Walsh <walshg3@go.stockton.edu>
2019-11-09 23:01:12 -0500

    finished week 6 project 2

--- DSSA-5001-Intro-To-DSSA/week_6/bookscript.sh
+++ DSSA-5001-Intro-To-DSSA/week_6/bookscript.sh
@@ -2,10 +2,9 @@
 # Greg Walsh
 # Created October 28th 2019
 # Last Edit: October 28th 2019
-# Details: Gets a book online saves to a .txt file. Cleans the information. Then will search for 5 of the keywords from parameters
-# Usage: Prompt$ ./bookscript booklink word1 word2 word3 word4 word5
-# Example: ./bookscript https://www.gutenberg.org/files/84/84-0.txt monster Frankenstein scared love hate and
-#xaa
+# Details: Gets a book online saves to a .txt file. Cleans the information. Then will search the Density or Actual Word count of words from parameters 
+# Usage: Prompt$ ./bookscript booklink density/actual word1 word2 word3 word4 word5 ... 
+# Example: ./bookscript https://www.gutenberg.org/files/84/84-0.txt density monster Frankenstein scared love hate and
 
 ## Curl a book file. 
 filename=$1
@@ -18,50 +17,74 @@ curl $filename > book.txt
 ## I am on a Mac so I needed this to do the same
 sed -i "" '/^[[:space:]]*$/d' book.txt
 ## split the file into files of 500 lines per 
-#wc -l book.txt 
 NUM_LINES=$(wc -l book.txt | awk '{print $1}')
-echo $NUM_LINES
 split -l $2 book.txt
 SPLITS=$(ls | grep xa) 
-echo $SPLITS
-## using a for loop get the density of the words per split file
 COUNTER=0
+x='||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||'
 
-for var in "${@:3}"
-do
-	echo "searching for the word: $var"
-	# Echo bar graphs with params from user
-	echo "|---5----10---15---20---25--->"
-	for file in $SPLITS
-	do  
-		#echo "file $COUNTER"
-		#echo "search var is $var"
-		#echo "search Split is $file"
-		WORD_COUNT=$(grep -o $var $file | wc -l)
-		#echo $WORD_COUNT
-		if (($WORD_COUNT == 0))
-		then
-			echo "NONE" 
-		else 
-			#PLOT_DATA=$(echo 'scale=3;'$WORD_COUNT / $2 | bc)
-			echo 'scale=3;'$WORD_COUNT / $2 | bc
-			#PLOT_DATA=$(echo 'scale=3;'$PLOT_DATA * 1000 | bc)
-			#echo $x | cut -c1-$DENSITY
-		fi
-		COUNTER=$((COUNTER+1))
-	done
-	echo "|---5----10---15---20---25--->"
-	COUNTER=0
-done 
 
-# Create Variable for the list of '|'
-x='|||||||||||||||||||||||||||||'
-
-# Echo bar graphs with params from user
-#echo "|---5----10---15---20---25--->"
-#echo $x | cut -c1-$1
-#echo $x | cut -c1-$2
-#echo $x | cut -c1-$3
-#echo $x | cut -c1-$4
-#echo $x | cut -c1-$5
-#echo "|---5----10---15---20---25--->"
+# If density is chosen run this block
+if [ $3 == "density" ]
+then
+	echo "USING DENSITY"
+	# for loop everything from 4th parameter (start of words)
+	for var in "${@:4}"
+	do
+		echo "searching for the word: $var"
+		# Echo bar graphs with params from user
+		echo "|---5----10---15---20---25---30---35---40---45---50---55---60---65---70---75---80---85---90---100-->"
+		for file in $SPLITS
+		do  
+			# Get Word Count from each split file per word.
+			WORD_COUNT=$(grep -o $var $file | wc -l)
+			if (($WORD_COUNT == 0))
+			then
+				echo "NA" 
+			else 
+				# Do some math. Density can not be a decimal so we must multiply by 1000 to ensure no decimal value
+				DENSITY=$(echo "$WORD_COUNT / $2" | bc -l)
+				PLOT_DATA=$(echo "$DENSITY * 1000 / 1" | bc)
+				if (( $PLOT_DATA >= 100 ))
+				then
+				echo "WARNING: Data is to high to graph actual value $PLOT_DATA"  
+				else
+				echo $x | cut -c1-$PLOT_DATA
+				fi
+			fi
+			COUNTER=$((COUNTER+1))
+		done
+		echo "|---5----10---15---20---25---30---35---40---45---50---55---60---65---70---75---80---85---90---100-->"
+		COUNTER=0
+	done 
+# If actual is chosen run this block
+elif [ $3 == "actual" ]
+then
+	echo "USING ACTUAL WORD COUNT"
+	for var in "${@:4}"
+	do
+		echo "searching for the word: $var"
+		# Echo bar graphs with params from user
+		echo "|---5----10---15---20---25---30---35---40---45---50---55---60---65---70---75---80---85---90---100-->"
+		for file in $SPLITS
+		do  
+			WORD_COUNT=$(grep -o $var $file | wc -l)
+			PLOT_DATA=$(echo "$WORD_COUNT / 1" | bc)
+			if (($WORD_COUNT == 0))
+			then
+				echo "NA" 
+			else 
+				if (( $PLOT_DATA >= 100 ))
+				then
+				echo "WARNING: Data is to high to graph. Actual value" $PLOT_DATA  
+				else
+				echo $x | cut -c1-$PLOT_DATA
+				fi
+			fi
+			COUNTER=$((COUNTER+1))
+		done
+		echo "|---5----10---15---20---25---30---35---40---45---50---55---60---65---70---75---80---85---90---100-->"
+		COUNTER=0
+	done 
+else echo "Error please chose density or actual"
+fi
--- DSSA-5101-DATA-EXPLORATION/Project3/Walsh_R3.r
+++ DSSA-5101-DATA-EXPLORATION/Project3/Walsh_R3.r
@@ -11,34 +11,40 @@
 # Create a bar graph using ggplot() of the Number of colonies per State with indication of number of colonies each Year
 # Create a line graph using ggplot() with each line representing a different state and indicating the Number of colonies per Year
 # Given the data, present a graph that you find interesting. You can use any graphing functions you want (e.g. plot, qplot, ggplot, plot.ly). It should be easy to recognize what you are presenting (thanks to titles, labels, and whatever else it needs). 
+## Set Working Directory
 setwd("/Users/gregwalsh/Github/DataScience /DSSA-5101-DATA-EXPLORATION/Project3")
+## Place Import Statements Here
 library(tidyverse)
 library(sqldf)
 library(ggplot2)
 ## Load DataSet 
 honeybee <- read.csv(file="HoneyBees.csv",sep=",",head=TRUE)
-## Create a basic plot of Yield per Colony against All Neonics
+## 1. Create a basic plot of Yield per Colony against All Neonics
 plot(honeybee$yieldpercol, honeybee$nAllNeonic)
-## Create a basic histogram of the number of honey producing colonies
+## 2. Create a basic histogram of the number of honey producing colonies
 hist(honeybee$numcol)
-## Create a plot using ggplot() or qplot() of Yield per Colony against All Neonics
+## 3. Create a plot using ggplot() or qplot() of Yield per Colony against All Neonics
 ggplot(honeybee, aes(x=yieldpercol,y=nAllNeonic)) + geom_point(binwidth = 10)
-## Create a histogram using ggplot() or qplot() of the number of honey producing colonies
+## 4. Create a histogram using ggplot() or qplot() of the number of honey producing colonies
 ggplot(honeybee, aes(x=numcol)) + geom_histogram(bins = 30)
-## Convert year, state, and Region into Factors
+## 5. Convert year, state, and Region into Factors
 year_factor <- factor(honeybee$year)
 state_factor <- factor(honeybee$state)
 region_factor <- factor(honeybee$Region)
-## Create a histogram using ggplot() of Total production of honey by State
+## 6. Create a histogram using ggplot() of Total production of honey by State
 ggplot(honeybee, aes(x=honeybee$state, y=honeybee$totalprod)) + geom_histogram(stat = "identity")
-## Create a bar graph using ggplot() of the Number of colonies by Year
+## 7. Create a bar graph using ggplot() of the Number of colonies by Year
 ggplot(honeybee, aes(x=honeybee$year, y=honeybee$numcol)) + geom_bar(stat = "identity")
-## Create a bar graph using ggplot() of the Number of colonies per State with indication of number of colonies each Year
+## 8. Create a bar graph using ggplot() of the Number of colonies per State with indication of number of colonies each Year
 ggplot(honeybee, aes(x=state_factor, y=honeybee$numcol, fill=year_factor)) + geom_bar(stat = "identity", position = "stack")
-## Create a line graph using ggplot() with each line representing a different state and indicating the Number of colonies per Year
+## 9. Create a line graph using ggplot() with each line representing a different state and indicating the Number of colonies per Year
 ggplot(honeybee, aes(x=honeybee$year, y=honeybee$numcol, group=honeybee$state)) + geom_line(aes(color=state_factor))
-## Given the data, present a graph that you find interesting. You can use any graphing functions you want (e.g. plot, qplot, ggplot, plot.ly). It should be easy to recognize what you are presenting (thanks to titles, labels, and whatever else it needs). 
+## 10. Given the data, present a graph that you find interesting. You can use any graphing functions you want (e.g. plot, qplot, ggplot, plot.ly). It should be easy to recognize what you are presenting (thanks to titles, labels, and whatever else it needs). 
 ## Question: Which states have the most agressive change in price per pound of honey per year?
-ggplot(honeybee, aes(x=honeybee$year,y=honeybee$priceperlb)) + geom_line() + facet_wrap(facets = vars(honeybee$state)) + ggtitle("Price per Pound of Honey By State Per Year") + labs(y="Price Per LB", x = "Year")
+## Used a facet_wrap to make the visualization a little cleaner
+ggplot(honeybee, aes(x=honeybee$year,y=honeybee$priceperlb)) + 
+  geom_line() + facet_wrap(facets = vars(honeybee$state)) + 
+  ggtitle("Price per Pound of Honey By State Per Year") + 
+  labs(y="Price Per LB", x = "Year")
 ## Answer NJ ME IL 
        
